a318ae004bea83e55d830a1245704b033456612d
Martina Manuel <msmartinamanuel@gmail.com>
2024-07-01 23:59:06 +0800

    Added javadocs comments

--- mini-project-3-final/src/main/java/org/cms/service/CartServiceImpl.java
+++ mini-project-3-final/src/main/java/org/cms/service/CartServiceImpl.java
@@ -5,14 +5,30 @@ import org.cms.model.Product;
 
 import java.util.Optional;
 
+/**
+ * Contains the business logic for the cart management system.
+ */
 public class CartServiceImpl implements CartService{
     Cart cart = new Cart();
 
+    /**
+     * Adds a product to cart. This method accepts an object of type Product object as argument and does not return
+     * any value.
+     *
+     * @param product
+     * @see Product
+     */
     @Override
     public void addProduct(Product product) {
         cart.getProductList().add(product);
     }
 
+    /**
+     * Removes product from cart. The argument accepted is the product ID of type String, which is used to filter the
+     * cart items. If a match is found, the instance of type Product is referenced by an optional object. If the
+     * optional object points to an existing product, it is removed, otherwise an error message is displayed.
+     * @param productId
+     */
     @Override
     public void removeProduct(String productId) {
         Optional<Product> forRemoval =  cart.getProductList()
@@ -24,6 +40,9 @@ public class CartServiceImpl implements CartService{
                 () -> System.out.println("This product is not in cart."));
     }
 
+    /**
+     * Displays the cart items in a tabular manner followed by the total price.
+     */
     @Override
     public void viewCart() {
         System.out.printf("%n%-20s %-10s %-36s%n", "Product Name", "Price", "Product ID");
@@ -33,6 +52,11 @@ public class CartServiceImpl implements CartService{
         System.out.println("Total Price: " + getTotalPrice());
     }
 
+    /**
+     * Returns the total price of all the items in the cart.
+     *
+     * @return total price
+     */
     @Override
     public double getTotalPrice() {
         return cart.getProductList().stream().mapToDouble(Product::getPrice).sum();
